045907fbfd1d0e8ceb5aba5c324461c1f09ec1e2
artluz <81848635+artluz@users.noreply.github.com>
2025-06-01 18:56:24 +0300

    ()_()

--- MAIN.sh
+++ MAIN.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "NOD.sh" ] || [ ! -f "NOK1.sh" ]; then
+if [ ! -f "script_NOD.sh" ] || [ ! -f "NOK1.sh" ]; then
     echo "Ошибка: скрипты NOD.sh или NOK1.sh не найдены в текущей директории!"
     exit 1
 fi
 
-if [ ! -x "NOD.sh" ] || [ ! -x "NOK1.sh" ]; then
+if [ ! -x "script_NOD.sh" ] || [ ! -x "NOK1.sh" ]; then
     echo "Делаем скрипты исполняемыми..."
     chmod +x NOD.sh NOK1.sh
 fi
