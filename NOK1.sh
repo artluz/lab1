6ef36b6602f0d04aa7be520e9b574714a207db29
artluz <81848635+artluz@users.noreply.github.com>
2025-06-01 19:17:20 +0300

    ------

--- NOK1.sh
+++ NOK1.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "Использование: $0 число1 число2"
+    exit 1
+fi
 
 gcd() {
-    local a=$1
-    local b=$2
+    a=$1; b=$2
     while [ $b -ne 0 ]; do
-        local temp=$b
+        temp=$b
         b=$((a % b))
         a=$temp
     done
     echo $a
 }
 
-
-lcm() {
-    local a=$1
-    local b=$2
-    local gcd_result=$(gcd $a $b)
-    echo $(( (a * b) / gcd_result ))
-}
-
-
-read -p "Введите первое число: " num1
-
-
-re='^[0-9]+$'
-if ! [[ $num1 =~ $re ]]; then
-    echo "Ошибка: введено не целое число"
-    exit 1
-fi
-
-
-read -p "Введите второе число: " num2
-
-
-if ! [[ $num2 =~ $re ]]; then
-    echo "Ошибка: введено не целое число"
-    exit 1
-fi
-
-
-result=$(lcm $num1 $num2)
-echo "НОК чисел $num1 и $num2 равен: $result"
+gcd_val=$(gcd $1 $2)
+echo "НОК $1 и $2: $(( ($1 * $2) / gcd_val ))"
